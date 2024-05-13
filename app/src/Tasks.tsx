a96537f8aefcf91b6d30c819ed840504c0f51cb1
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-05-13 12:44:29 +0900

    feature: ユーザーのアクションをトリガーにリクエストを送る

--- app/src/Tasks.tsx
+++ app/src/Tasks.tsx
@@ -1,28 +1,51 @@
-import { useQuery } from "@tanstack/react-query";
-import { getTasks, Task } from "./repository.ts";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createTask, getTasks, Task } from "./repository.ts";
 
-export const Tasks = () => {
-  const query = useQuery({
+const useTasks = () => {
+  return useQuery({
     queryKey: ["tasks"],
     queryFn: getTasks,
   });
+};
 
-  if (query.isLoading) {
+const useCreateTask = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createTask,
+    onSuccess: () => {
+      queryClient.refetchQueries(["tasks"]);
+    },
+  });
+};
+
+export const Tasks = () => {
+  const fetchTasks = useTasks();
+  const createTask = useCreateTask();
+
+  if (fetchTasks.isLoading) {
     return <h1>Loading...</h1>;
   }
 
-  if (query.isError) {
-    return <h1>Error: {String(query.error)}</h1>;
+  if (fetchTasks.isError) {
+    return <h1>Error: {String(fetchTasks.error)}</h1>;
   }
 
   return (
     <main className="container mx-auto flex flex-col gap-8 p-8 text-white">
       <h1 className="text-7xl">Tasks</h1>
       <ul className="flex flex-col gap-4">
-        {query.data.tasks.map((task: Task, index) => (
+        {fetchTasks.data.tasks.map((task: Task, index) => (
           <li key={index}>{task.title}</li>
         ))}
       </ul>
+      <button
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded w-fit self-center"
+        onClick={() => createTask.mutate()}
+      >
+        Create Task
+      </button>
     </main>
   );
 };
